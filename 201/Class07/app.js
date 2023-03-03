73806a43bcd63cbd537c1bd2334925175d8676af
Kelsee Watts <123975076+chillgatez@users.noreply.github.com>
2023-03-03 12:12:58 -0600

    deactivating script

--- 201/Class07/app.js
+++ 201/Class07/app.js
@@ -1,5 +1,6 @@
 'use strict'
-//random function between max and min input
+
+/* random function between max and min input
 
 let hoursDemo = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
 
